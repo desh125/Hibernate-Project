b5aa21f9959e1d3b465e58e8af5c655f4366f5c0
desh125 <deshtharaka@gmail.com>
2023-08-28 09:47:52 +0530

    Second Commit

---
+++ src/lk/ijse/orm/hibernate_project/entities/Student.java
@@ -0,0 +1,67 @@
+package lk.ijse.orm.hibernate_project.entities;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "student_id")
+    private Long id;
+
+    @Column(name = "student_name")
+    private String name;
+
+    @Column(name = "campus_id")
+    private String campusId;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "registration_date")
+    private Date registrationDate;
+
+    public Student() {
+    }
+
+
+    public Student(Long id, String name, String campusId, Date registrationDate) {
+        this.id = id;
+        this.name = name;
+        this.campusId = campusId;
+        this.registrationDate = registrationDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCampusId() {
+        return campusId;
+    }
+
+    public void setCampusId(String campusId) {
+        this.campusId = campusId;
+    }
+
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Date registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+}
